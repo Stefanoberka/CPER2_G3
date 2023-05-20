d683219298b82fd58effe4770c628fc8daf37a2e
Federico Mattiuzzi <79696658+JLarn@users.noreply.github.com>
2023-05-20 12:43:29 +0200

    rinominate classi

---
+++ Api/CPER2G3.Earth4Sport.AzureFunction/Models/DeviceData.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace CPER2G3.Earth4Sport.AzureFunction.Models
+{
+    public class DeviceData
+    {
+        [BsonId]
+        [BsonElement("_id")]
+        public string uuid { get; set; }
+        [BsonElement("n_batch")]
+        public int n_batch { get; set; }
+        [BsonElement("data_batch")]
+        public DateTime data_batch { get; set; }
+    }
+}
--- Api/CPER2G3.Earth4Sport.AzureFunction/Models/SessionData.cs
+++ Api/CPER2G3.Earth4Sport.AzureFunction/Models/SessionData.cs
@@ -1,15 +1,23 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using System;
 
 namespace CPER2G3.Earth4Sport.AzureFunction.Models {
-    public class ClockActivityData {
+    public class SessionData {
+        [BsonId]
         public ObjectId _id {  get; set; }
+        [BsonElement("SessionUUID")]
         public string SessionUUID { get; set; }
+        [BsonElement("Pools")]
         public int Pools { get; set; }
+        [BsonElement("Distance")]
         public double Distance { get; set; }
+        [BsonElement("Bpm")]
         public int Bpm { get; set; }
+        [BsonElement("Gps")]
         public Gps Gps { get; set; }
+        [BsonElement("TimeStamp")]
         public DateTime TimeStamp { get; set; }
     }
 
