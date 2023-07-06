af393c9fd55bf07b36b5c28115fa7a14af967e24
Raffaele Puntin <raffaele.puntin@stud.itsaltoadriatico.it>
2023-07-06 11:29:19 +0200

    Sorted clocks data requests

--- CPER2G3.Earth4Sport.AzureFunction/Service/DAL_mongo.cs
+++ CPER2G3.Earth4Sport.AzureFunction/Service/DAL_mongo.cs
@@ -6,21 +6,19 @@ using MongoDB.Driver;
 using MongoDB.Driver.Linq;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 
-namespace CPER2G3.Earth4Sport.AzureFunction.Service
-{
-    public class DAL_mongo : IDAL
-    {
+namespace CPER2G3.Earth4Sport.AzureFunction.Service {
+    public class DAL_mongo : IDAL {
         private readonly string _connectionString;
         private MongoClient client;
         private IMongoDatabase dbProvisioning;
         private IMongoDatabase dbSessions;
 
-        public DAL_mongo(IConfiguration conf)
-        {
+        public DAL_mongo(IConfiguration conf) {
             _connectionString = conf.GetConnectionString("mongo");
             client = new MongoClient(_connectionString);
             dbProvisioning = client.GetDatabase("provisioning");
@@ -28,73 +26,57 @@ namespace CPER2G3.Earth4Sport.AzureFunction.Service
         }
 
         #region ClockSessions
-        public async Task<ObjectResult> getSessionsList(string clockUuid)
-        {
+        public async Task<ObjectResult> getSessionsList(string clockUuid) {
             var AllSessionsData = dbSessions.GetCollection<ActivityData>(clockUuid);
-            if (AllSessionsData == null)
-            {
+            if (AllSessionsData == null) {
                 return new NotFoundObjectResult("Nessun dato.");
             }
-            try
-            {
+            try {
                 List<SessionSummary> res = new List<SessionSummary>();
                 var sessionList = AllSessionsData.AsQueryable().Select(d => d.SessionUUID).Distinct().ToList();
-                foreach (var session in sessionList)
-                {
-                    var data = AllSessionsData.Find(s => s.SessionUUID == session).ToList();
+                foreach (var session in sessionList) {
+                    var data = AllSessionsData.Find(s => s.SessionUUID == session).SortBy(s => s.TimeStamp).ToList();
                     SessionSummary summary = new SessionSummary(data, session);
-                    if (summary.TotalDistance != 0)
-                    {
+                    if (summary.TotalDistance != 0) {
                         res.Add(summary);
                     }
                 }
-                res.OrderByDescending(s => s.Start);
+                res = res.OrderBy(s => s.Start).ToList();
                 return new ObjectResult(res);
             }
-            catch (Exception e)
-            {
+            catch (Exception e) {
                 return new ObjectResult(e.Message);
             }
         }
-        public async Task<ObjectResult> getSessionActivities(string sessionUuid, string clockUuid)
-        {
+        public async Task<ObjectResult> getSessionActivities(string sessionUuid, string clockUuid) {
             var collection = dbSessions.GetCollection<ActivityData>(clockUuid);
-            if (collection == null)
-            {
+            if (collection == null) {
                 return new NotFoundObjectResult("L'orologio non esiste");
             }
-            try
-            {
-                var sessionCollection = collection.Find(s => s.SessionUUID == sessionUuid).ToList();
+            try {
+                var sessionCollection = collection.Find(s => s.SessionUUID == sessionUuid).SortBy(s => s.TimeStamp).ToList();
 
-                sessionCollection.OrderByDescending(s => s.TimeStamp);
-                if (sessionCollection == null)
-                {
+                if (sessionCollection == null) {
                     return new NotFoundObjectResult("La sessione non esiste");
                 }
                 return new ObjectResult(sessionCollection);
             }
-            catch (Exception e)
-            {
+            catch (Exception e) {
                 return new ObjectResult(e.Message);
             }
         }
 
-        public async Task<ObjectResult> postClock(ActivityData activity, string clockUuid)
-        {
+        public async Task<ObjectResult> postClock(ActivityData activity, string clockUuid) {
             var collection = dbSessions.GetCollection<ActivityData>(clockUuid);
-            if (collection == null)
-            {
+            if (collection == null) {
                 dbProvisioning.CreateCollection(clockUuid);
                 collection = dbSessions.GetCollection<ActivityData>(clockUuid);
             }
-            try
-            {
+            try {
                 await collection.InsertOneAsync(activity);
                 return new OkObjectResult("Inserimento avvenuto".ToJson());
             }
-            catch (Exception)
-            {
+            catch (Exception) {
                 return new BadRequestObjectResult("Errore");
             }
         }
